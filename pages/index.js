66a613fe6bcfdb585e6919c1e94d0093f5d70875
Greg <greg_markarian@hotmail.com>
2022-01-12 16:50:48 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+import cn from "classnames";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={cn("font-bold")}>asdsadsad</h1>
+    </div>
+  );
+}
