e3cb7d0fe3016429a75417fc1f2cb024c2e03cef
Greg <greg_markarian@hotmail.com>
2022-01-14 17:43:27 +0200

    update

---
+++ src/Contetnt.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CoffeCard from "./CoffeCard";
+
+const Contetnt = () => {
+  return (
+    <div>
+      <CoffeCard />
+    </div>
+  );
+};
+
+export default Contetnt;
