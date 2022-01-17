0f7daefb56305c2fc532fa6cdedc8df30c1d47bc
Greg <greg_markarian@hotmail.com>
2022-01-17 11:25:05 +0200

    update

--- src/CoffeCard.js
+++ src/CoffeCard.js
@@ -43,11 +43,7 @@ const CoffeCard = (props) => {
         alt='Paella dish'
       />
       <CardContent>
-        <Typography variant='body2'>
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Typography variant='body2'>{description}</Typography>
       </CardContent>
       <CardActions>
         <Button size='small'>Learn More</Button>
