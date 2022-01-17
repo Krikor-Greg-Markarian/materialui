0689da8b4a3b565c39679e946a3e6bd583b17f3f
Greg <greg_markarian@hotmail.com>
2022-01-17 11:48:51 +0200

    update

--- src/CoffeCard.js
+++ src/CoffeCard.js
@@ -71,6 +71,7 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button } from "@mui/material";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -83,10 +84,10 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function RecipeReviewCard() {
+export default function RecipeReviewCard(props) {
   const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
+  const handleExpandClick = (props) => {
     setExpanded(!expanded);
   };
 
@@ -95,37 +96,39 @@ export default function RecipeReviewCard() {
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
-            R
+            {props.AvatarName}
           </Avatar>
         }
         action={
           <IconButton aria-label='settings'>
-            <MoreVertIcon />
+            <ShareIcon />
           </IconButton>
         }
-        title={title}
-        subheader={subHeader}
+        title={props.title}
+        subheader={props.subHeader}
       />
       <CardMedia
         component='img'
         height='194'
-        image={imageUrl}
+        image={props.imageUrl}
         alt='Paella dish'
       />
       <CardContent>
         <Typography variant='body2' color='text.secondary'>
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {props.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label='add to favorites'>
+        {/* <IconButton aria-label='add to favorites'>
           <FavoriteIcon />
         </IconButton>
         <IconButton aria-label='share'>
           <ShareIcon />
-        </IconButton>
+        </IconButton> */}
+        <CardActions>
+          <Button size='small'>BUY NOW</Button>
+          <Button size='small'>ORDER</Button>
+        </CardActions>
         {/* <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
--- src/Contetnt.js
+++ src/Contetnt.js
@@ -2,7 +2,7 @@ import React from "react";
 import CoffeCard from "./CoffeCard";
 import { Grid } from "@mui/material";
 
-const Contetnt = () => {
+const Contetnt = (props) => {
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} sm={4}>
@@ -12,22 +12,51 @@ const Contetnt = () => {
           // avatarSrc='https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg'
           // imageUrl='https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466__340.jpg'
           // description='Voluptates iusto enim odit possimus neque. Saepe quos quasi non tempore ea! Aliquid obcaecati quo fugit ipsum! Nam beatae neque, consectetur id, quia saepe nihil voluptatibus.'
+          AvatarName='A'
+          title='LB Single Serve Coffee'
+          subHeader='$37.99'
+          imageUrl='https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466__340.jpg'
         />
       </Grid>
       <Grid item xs={12} sm={4}>
-        <CoffeCard />
+        <CoffeCard
+          AvatarName='B'
+          title='LB Single Serve Coffee'
+          subHeader='$32'
+          imageUrl='https://cdn.pixabay.com/photo/2015/05/04/10/16/vegetables-752153__340.jpg'
+        />
       </Grid>
       <Grid item xs={12} sm={4}>
-        <CoffeCard />
+        <CoffeCard
+          AvatarName='C'
+          title='LB Single Serve Coffee'
+          subHeader='$33.43'
+          imageUrl='https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467__340.jpg'
+        />
       </Grid>
       <Grid item xs={12} sm={4}>
-        <CoffeCard />
+        <CoffeCard
+          AvatarName='D'
+          title='LB Single Serve Coffee'
+          subHeader='$23.99'
+          imageUrl='https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__340.jpg'
+        />
       </Grid>
       <Grid item xs={12} sm={4}>
-        <CoffeCard />
+        <CoffeCard
+          AvatarName='E'
+          title='LB Single Serve Coffee'
+          subHeader='$13.99'
+          imageUrl='https://cdn.pixabay.com/photo/2017/12/10/14/47/pizza-3010062__340.jpg'
+        />
       </Grid>
       <Grid item xs={12} sm={4}>
-        <CoffeCard />
+        <CoffeCard
+          AvatarName='F'
+          title='LB Single Serve Coffee'
+          subHeader='$8'
+          imageUrl='https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590__480.jpg'
+        />
       </Grid>
     </Grid>
   );
