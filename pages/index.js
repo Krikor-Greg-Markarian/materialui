99480699b3572657b09f63ec87ffc8b45c466559
Greg <greg_markarian@hotmail.com>
2022-01-17 10:54:46 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home(props) {
         <Header />
       </Grid>
       <Grid item container>
-        <Grid item xs={0} sm={2} />
+        <Grid item xs={false} sm={2} />
         <Grid item xs={12} sm={8}>
           <Contetnt />
         </Grid>
-        <Grid item xs={0} sm={2} />
+        <Grid item xs={false} sm={2} />
       </Grid>
     </Grid>
   );
--- src/Contetnt.js
+++ src/Contetnt.js
@@ -1,11 +1,29 @@
 import React from "react";
 import CoffeCard from "./CoffeCard";
+import { Grid } from "@mui/material";
 
 const Contetnt = () => {
   return (
-    <div>
-      <CoffeCard />
-    </div>
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={4}>
+        <CoffeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeCard />
+      </Grid>
+    </Grid>
   );
 };
 
