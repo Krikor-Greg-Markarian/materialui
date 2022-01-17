fd78e85a66c809a04e3e3e08a99a1be15557c7ea
Greg <greg_markarian@hotmail.com>
2022-01-17 13:20:08 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import { Grid } from "@material-ui/core";
 import Header from "../src/Header";
 import Contetnt from "../src/Contetnt";
+import { Button } from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  buttonStyle: {
+    // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    // border: 0,
+    // borderRadius: 3,
+    // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    // color: "white",
+    // height: 48,
+    // width: 220,
+    // padding: "0 30px",
+    color: "red",
+  },
+});
 
 export default function Home(props) {
+  const classes = useStyles();
   return (
     <Grid container direction='column'>
       <Grid item>
@@ -15,6 +32,7 @@ export default function Home(props) {
         </Grid>
         <Grid item xs={false} sm={2} />
       </Grid>
+      <Button className={classes.buttonStyle}>Testing button</Button>
     </Grid>
   );
 }
