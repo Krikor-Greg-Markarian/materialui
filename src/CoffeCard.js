29736c6769b41e922f32b18c6b296225f94afee2
Greg <greg_markarian@hotmail.com>
2022-01-17 11:19:33 +0200

    update

--- src/CoffeCard.js
+++ src/CoffeCard.js
@@ -43,15 +43,6 @@ const CoffeCard = (props) => {
         alt='Paella dish'
       />
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant='h5' component='div'>
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-          adjective
-        </Typography>
         <Typography variant='body2'>
           well meaning and kindly.
           <br />
